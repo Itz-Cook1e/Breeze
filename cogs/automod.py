f2033dac0be11ba1e256791edd734751662bde88
Itz Cook1e <Asherpflueger@outlook.com>
2021-10-01 23:45:09 -0500

    Update automod.py

--- cogs/automod.py
+++ cogs/automod.py
@@ -1,11 +1,16 @@
+# Import libraries
 import discord
 from discord.ext import commands
 
+# Create class to define bot and load it in as a cog
 class AutoMod(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
 
+    
+# No command yet :(
+    
 
-
+# Load up as a cog
 def setup(bot):
     bot.add_cog(AutoMod(bot))
